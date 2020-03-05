00be0db0560be7a5f465c914128360f3644779a1
Guillaume R <guillaume.rondon@gmail.com>
2020-03-05 20:49:08 +0100

    react todo avec bootstrap et font awesome

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {FaListAlt, FaCheckSquare, FaPlusSquare, FaTrash} from 'react-icons/fa'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+    render() {
+        return (
+            <section id="todo">
+                <h1 className="m-3">Liste de tâches</h1>
+                <ul className="list-group m-3">
+                    <li className="list-group-item d-flex align-tiems-center">
+                        Ranger la vaisselle
+                <button className="btn btn-sm ml-auto btn-outline-success">&#x2713;</button>
+                    </li>
+                    <li className="list-group-item d-flex align-tiems-center">
+                        Répondre appel d'offres
+                <button className="btn btn-sm ml-auto btn-outline-success">&#x2713;</button>
+                    </li>
+                    <li className="list-group-item d-flex align-tiems-center">
+                        Signer contrat
+                <button className="btn btn-sm ml-auto btn-outline-success">&#x2713;</button>
+                    </li>
+                    <li className="list-group-item d-flex align-tiems-center">
+                        Ranger la salon
+                <button className="btn btn-sm ml-auto btn-outline-success">&#x2713;</button>
+                    </li>
+                </ul>
+                <footer className="d-flex justify-content-between bg-secondary p-3" id="mainFooter">
+                    <div className="btn-group">
+                        <a href="#" className="btn btn-outline-dark bg-light"><FaListAlt /></a>
+                        <a href="#" className="btn btn-outline-dark bg-light"><FaCheckSquare /></a>
+                        <a href="#" className="btn btn-outline-dark bg-light"><FaPlusSquare /></a>
+                    </div>
+                    <button className="btn btn-outline-dark bg-light"><FaTrash /></button>
+                </footer>
+            </section>
+        )
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById('root'))
